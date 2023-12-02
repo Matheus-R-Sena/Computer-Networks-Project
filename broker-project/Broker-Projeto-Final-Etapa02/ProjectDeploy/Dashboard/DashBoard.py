445a56cc46d6597a21f17941d1500297016d891c
Matheus da Rocha Sena <93345712+Matheus-R-Sena@users.noreply.github.com>
2023-12-02 15:22:19 -0300

    Atualizações

--- broker-project/Broker-Projeto-Final-Etapa02/ProjectDeploy/Dashboard/DashBoard.py
+++ broker-project/Broker-Projeto-Final-Etapa02/ProjectDeploy/Dashboard/DashBoard.py
@@ -1,4 +1,6 @@
 #Dashboard apenas com a parte lógica pronta
+# Comando para rodar o dashboard no navegador: 
+# streamlit run .\DashBoard.py
 
 import socket             # biblioteca para estabelecer as conexões
 import threading          # biblioteca para criar as threads
@@ -9,7 +11,7 @@ import streamlit as st    # biblioteca para criar o dashboard web
 topicos = ["Clima", "Temperatura", "Umidade"]
 
 # lista com os valores dos topicos
-variacoes = ["", 0, 0]
+variacoes = [0, 0, 0]
 
 # funcao para assinar os topicos
 def comunicacao(servidor):
@@ -27,21 +29,21 @@ def comunicacao(servidor):
             # cria uma thread para os assinantes
             if mensagem.startswith("Clima"):
                 contador = contador + 1
-                threadAssinante = threading.Thread(target = AtualizaClima, args = (cliente,))
-                threadAssinante.start()
+                threadClima = threading.Thread(target = AtualizaClima, args = (cliente,))
+                threadClima.start()
  
             # cria uma thread para publicar as mensagens
             elif mensagem.startswith("Temperatura"):
                 contador = contador + 1
-                threadPublicacao = threading.Thread(target = AtualizaTemperatura, args = (cliente,))
-                threadPublicacao.start()
+                threadTemperatura = threading.Thread(target = AtualizaTemperatura, args = (cliente,))
+                threadTemperatura.start()
         
 
             # chama a função para listar os topicos e seus assinantes
             elif mensagem.startswith("Umidade"):
                 contador = contador + 1
-                threadPublicacao = threading.Thread(target = AtualizaUmidade, args = (cliente,))
-                threadPublicacao.start()
+                threadUmidade = threading.Thread(target = AtualizaUmidade, args = (cliente,))
+                threadUmidade.start()
 
     except Exception as e:
         print(f"Erro na conexão servidor: {e}") # exceção caso a conexão não seja feita
@@ -53,9 +55,16 @@ def AtualizaClima (cliente):
         #for topico in topicos:
             #if topico == "Clima":
             mensagem = cliente.recv(1024).decode()
+            #print(mensagem)
             print(mensagem)
-            variacoes[0] = mensagem
 
+            if mensagem == "nublado":
+                variacoes[0] = 1
+            elif mensagem == "ensolarado":
+                variacoes[0] = 2
+            elif mensagem == "chuvoso":
+                variacoes[0] = 3
+            
 
 
 def AtualizaTemperatura (cliente):
@@ -64,8 +73,9 @@ def AtualizaTemperatura (cliente):
     #for topico in topicos:
         #if topico == "Temperatura":
         mensagem = cliente.recv(1024).decode()
-        print(mensagem)
+        #print(mensagem)
         variacoes[1] = mensagem
+        print(variacoes[1])
 
 def AtualizaUmidade (cliente):
     
@@ -74,11 +84,36 @@ def AtualizaUmidade (cliente):
     #for topico in topicos:
         #if topico == "Umidade":
         mensagem = cliente.recv(1024).decode()
-        print(mensagem)
+        #print(mensagem)
         variacoes[2] = mensagem
+        print(variacoes[2])
+
+
+
+def frasesClima (Clima):
+
+    if Clima == 1:
+        fraseClima.markdown(f'<p style="color: orange; font-size: 1.8em; text-align: center;"> nublado </p>', unsafe_allow_html=True)
+
+    elif Clima == 2:
+        fraseClima.markdown(f'<p style="color: orange; font-size: 1.8em; text-align: center;"> ensolarado </p>', unsafe_allow_html=True)
+
+    elif Clima == 3:
+        fraseClima.markdown(f'<p style="color: orange; font-size: 1.8em; text-align: center;"> chuvoso </p>', unsafe_allow_html=True)
+
+   
+
 
 # funcoes para mudar as frases dos topicos
 def frasesTemperatura (temperatura):
+
+    #Para transformar string em um número
+    try:
+        temperatura = float(temperatura)
+    except ValueError:
+        # Se a conversão falhar, defina a temperatura como 0 ou outro valor padrão
+        temperatura = 0
+
 
     if temperatura <= 20:
         fraseTemperatura.markdown(f'<p style="color: orange; font-size: 1.8em; text-align: center;"> Está frio, coloca uma meia. Ass: Mãe </p>', unsafe_allow_html=True)
@@ -98,45 +133,33 @@ def frasesTemperatura (temperatura):
     else:
         fraseTemperatura.markdown(f'<p style="color: orange; font-size: 1.8em; text-align: center;"> Já fomos com Deus. </p>', unsafe_allow_html=True)
 
-def frasesVento (vento):
 
-    if vento <= 20:
-        fraseVento.markdown(f'<p style="color: lightgreen; font-size: 1.8em; text-align: center;"> Ventinho agradável. </p>', unsafe_allow_html=True)
+def frasesUmidade (Umidade):
 
-    elif 20 < vento <= 30:
-        fraseVento.markdown(f'<p style="color: lightgreen; font-size: 1.8em; text-align: center;"> Segura os papéis senão voa tudo. </p>', unsafe_allow_html=True)
+    #Para transformar string em um número
+    try:
+        Umidade = float(Umidade)
+    except ValueError:
+        # Se a conversão falhar, defina a temperatura como 0 ou outro valor padrão
+        Umidade = 0
 
-    elif 30 < vento <= 40:
-        fraseVento.markdown(f'<p style="color: lightgreen; font-size: 1.8em; text-align: center;"> Cheio de folhas no chão, olha lá. </p>', unsafe_allow_html=True)
+    if Umidade <= 25:
+        fraseUmidade.markdown(f'<p style="color: lightblue; font-size: 1.8em; text-align: center;"> Céu limpo, tem chuva não. </p>', unsafe_allow_html=True)
 
-    elif 40 < vento <= 50:
-        fraseVento.markdown(f'<p style="color: lightgreen; font-size: 1.8em; text-align: center;"> Melhor ficar em casa mesmo. </p>', unsafe_allow_html=True)
+    elif 25 < Umidade <= 40:
+        fraseUmidade.markdown(f'<p style="color: lightblue; font-size: 1.8em; text-align: center;"> Coloca um guarda chuva na bolsa por precaução. </p>', unsafe_allow_html=True)
 
-    elif 50 < vento <= 70:
-       fraseVento.markdown(f'<p style="color: lightgreen; font-size: 1.8em; text-align: center;"> Pelo amor de Deus não sai de casa! </p>', unsafe_allow_html=True)
+    elif 40 < Umidade <= 50:
+        fraseUmidade.markdown(f'<p style="color: lightblue; font-size: 1.8em; text-align: center;"> Talvez venha chuva por aí. </p>', unsafe_allow_html=True)
 
-    else:
-        fraseVento.markdown(f'<p style="color: lightgreen; font-size: 1.8em; text-align: center;"> O telhado foi de arrasta para cima. </p>', unsafe_allow_html=True)
+    elif 50 < Umidade <= 60:
+        fraseUmidade.markdown(f'<p style="color: lightblue; font-size: 1.8em; text-align: center;"> Será que vale a pena sair de casa hoje? </p>', unsafe_allow_html=True)
 
-def frasesChuva (chuva):
-
-    if chuva <= 25:
-        fraseChuva.markdown(f'<p style="color: lightblue; font-size: 1.8em; text-align: center;"> Céu limpo, tem chuva não. </p>', unsafe_allow_html=True)
-
-    elif 25 < chuva <= 40:
-        fraseChuva.markdown(f'<p style="color: lightblue; font-size: 1.8em; text-align: center;"> Coloca um guarda chuva na bolsa por precaução. </p>', unsafe_allow_html=True)
-
-    elif 40 < chuva <= 50:
-        fraseChuva.markdown(f'<p style="color: lightblue; font-size: 1.8em; text-align: center;"> Talvez venha chuva por aí. </p>', unsafe_allow_html=True)
-
-    elif 50 < chuva <= 60:
-        fraseChuva.markdown(f'<p style="color: lightblue; font-size: 1.8em; text-align: center;"> Será que vale a pena sair de casa hoje? </p>', unsafe_allow_html=True)
-
-    elif 60 < chuva <= 80:
-       fraseChuva.markdown(f'<p style="color: lightblue; font-size: 1.8em; text-align: center;"> Melhor já tirar as roupas do varal. </p>', unsafe_allow_html=True)
+    elif 60 < Umidade <= 80:
+       fraseUmidade.markdown(f'<p style="color: lightblue; font-size: 1.8em; text-align: center;"> Melhor já tirar as roupas do varal. </p>', unsafe_allow_html=True)
 
     else:
-        fraseChuva.markdown(f'<p style="color: lightblue; font-size: 1.8em; text-align: center;"> Se você sair de casa vai chover, acredite. </p>', unsafe_allow_html=True)
+        fraseUmidade.markdown(f'<p style="color: lightblue; font-size: 1.8em; text-align: center;"> Se você sair de casa vai chover, acredite. </p>', unsafe_allow_html=True)
 
 
 # formatacoes do dashboard
@@ -145,12 +168,14 @@ st.write("\n") # espaço em branco
 
 # topico 1
 st.markdown('<p style="color: orange; font-size: 2em;"> Clima </p>', unsafe_allow_html=True)  # titulo do topico 1
-# Substitua a linha da barra de progresso por um texto
-st.text(f'Clima: {variacoes[0]}')  # valor atual do topico
+ 
+
+#linha1 = st.progress(0)  # cria a linha 1
+#numero1 = st.markdown('')  # valor atual do topico
 
 st.write("\n")  # espaço em branco
 
-fraseTemperatura = st.markdown('')  # frase atual do topico
+fraseClima = st.markdown('')  # frase atual do topico
 
 st.write("\n")  # espaço em branco
 st.write("\n")  # espaço em branco
@@ -162,13 +187,13 @@ st.text("0                                                                      
 linha2 = st.progress(0)  # cria a linha 2
 numero2 = st.markdown('')  # valor atual do topico 
 
-st.write("\n")  # espaço em branco
-
-fraseVento = st.markdown('')  # frase atual do topico
+fraseTemperatura = st.markdown('')  # frase atual do topico
 
 st.write("\n")  # espaço em branco
 st.write("\n")  # espaço em branco
 st.write("\n") # espaço em branco
+
+st.write("\n")  # espaço em branco
 
 # topico 3
 st.markdown('<p style="color: lightblue; font-size: 2em;"> Umidade</p>', unsafe_allow_html=True)  # titulo do topico 3
@@ -178,14 +203,18 @@ numero3 = st.markdown('')  # valor atual do topico
 
 st.write("\n") # espaço em branco
 
-fraseChuva = st.markdown('')  # frase atual do topico
+fraseUmidade = st.markdown('')  # frase atual do topico
+
+st.write("\n")  # espaço em branco
+st.write("\n")  # espaço em branco
+st.write("\n") # espaço em branco
 
 
 # for para criar as threads de assinantes
 servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 servidor.bind(("127.0.0.1", 10000))
 servidor.listen()
-print("Servidor escutando na porta 10000")
+print("Servidor escutando na porta 15000")
 
 comunicacao(servidor)
 
@@ -196,19 +225,24 @@ comunicacao(servidor)
 while True:
 
     # atualiza as linhas de cada topico
-    v1 = float(variacoes[1])
-    v2 = float(variacoes[2])
-    linha2.progress(v1 / 100.0)
-    linha3.progress(v2 / 100.0)
+    v1 = float(variacoes[0])
+    v2 = float(variacoes[1])
+    v3 = float(variacoes[2])
+
+    #linha1.progress(v1 / 100.0)
+    linha2.progress(v2 / 100.0)
+    linha3.progress(v3 / 100.0)
 
     # atualiza os valores de cada topico
-    
+
+    #numero1.markdown(f'<p style="color: orange; font-size: 1.8em; text-align: center;"> {str(variacoes[0])} </p>', unsafe_allow_html=True)
     numero2.markdown(f'<p style="color: lightgreen; font-size: 1.8em; text-align: center;"> {str(variacoes[1])} </p>', unsafe_allow_html=True)
     numero3.markdown(f'<p style="color: lightblue; font-size: 1.8em; text-align: center;"> {str(variacoes[2])} </p>', unsafe_allow_html=True)
 
     # atualiza as frases de cada topico
-    frasesVento(variacoes[1])
-    frasesChuva(variacoes[2])
+    frasesTemperatura (variacoes[0])
+    frasesTemperatura(variacoes[1])
+    frasesUmidade(variacoes[2])
     
-    time.sleep(1)
+    time.sleep(2)
    
